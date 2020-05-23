c77cae5f481d657295059aedb8039d5cd4a0461e
Diana Baburina <dbaburina.consultant@newscorp.com>
2020-05-24 01:54:12 +0300

    Styling issues

--- src/components/CreateGoalModal/CreateForm.jsx
+++ src/components/CreateGoalModal/CreateForm.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Form, Input } from 'semantic-ui-react';
+import { Form, Input, Grid } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import GroupsSelect from '../ConnectCardModal/GroupsSelect';
 
@@ -21,61 +22,79 @@ export default class CreateGoalForm extends React.Component {
 
     return (
       <Form>
-        <GroupsSelect
-          selectedGroup={groupId}
-          handleGroupSelect={(e, { value }) => handleInputChange('groupId', value)}
-          {...(errorField === 'groupId' && { selectGroupError: errorMessage })}
-        />
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column>
 
-        <Form.Field
-          control={Input}
-          label="Name"
-          placeholder="Name"
-          defaultValue={name}
-          onChange={(e, { value }) => handleInputChange('name', value)}
-          {...(errorField === 'name' ? { error } : {})}
-        />
+              <GroupsSelect
+                selectedGroup={groupId}
+                handleGroupSelect={(e, { value }) => handleInputChange('groupId', value)}
+                {...(errorField === 'groupId' && { selectGroupError: errorMessage })}
+              />
 
-        <Form.Field
-          control={Input}
-          label="Description"
-          placeholder="Description"
-          defaultValue={description}
-          onChange={(e, { value }) => handleInputChange('description', value)}
-          {...(errorField === 'description' ? { error } : {})}
-        />
+              <Form.Field
+                control={Input}
+                label="Name"
+                placeholder="Name"
+                defaultValue={name}
+                onChange={(e, { value }) => handleInputChange('name', value)}
+                {...(errorField === 'name' ? { error } : {})}
+              />
 
-        <Form.Field
-          control={Input}
-          type="number"
-          label="Amount"
-          placeholder="Amount"
-          defaultValue={amount}
-          onChange={(e, { value }) => handleInputChange('amount', value)}
-          {...(errorField === 'amount' ? { error } : {})}
-        />
+              <Form.Field
+                control={Input}
+                label="Description"
+                placeholder="Description"
+                defaultValue={description}
+                onChange={(e, { value }) => handleInputChange('description', value)}
+                {...(errorField === 'description' ? { error } : {})}
+              />
 
-        <Form.Field
-          control={Input}
-          type="number"
-          label="Save per month"
-          placeholder="Save per month"
-          defaultValue={savePerMonth}
-          onChange={(e, { value }) => handleInputChange('savePerMonth', value)}
-          {...(errorField === 'savePerMonth' ? { error } : {})}
-        />
+              <Form.Field
+                control={Input}
+                type="number"
+                label="Amount"
+                placeholder="Amount"
+                defaultValue={amount}
+                onChange={(e, { value }) => handleInputChange('amount', value)}
+                {...(errorField === 'amount' ? { error } : {})}
+              />
 
-        <DatePicker
-          dateFormat="MMMM d, yyyy"
-          selected={new Date(dateStart)}
-          onChange={(date) => handleInputChange('dateStart', date)}
-        />
+              <Form.Field
+                control={Input}
+                type="number"
+                label="Save per month"
+                placeholder="Save per month"
+                defaultValue={savePerMonth}
+                onChange={(e, { value }) => handleInputChange('savePerMonth', value)}
+                {...(errorField === 'savePerMonth' ? { error } : {})}
+              />
 
-        <DatePicker
-          dateFormat="MMMM d, yyyy"
-          selected={new Date(dateEnd)}
-          onChange={(date) => handleInputChange('dateEnd', date)}
-        />
+
+            </Grid.Column>
+            <Grid.Column>
+
+              <div className="field">
+                <label>Start date</label>
+                <DatePicker
+                  dateFormat="MMMM d, yyyy"
+                  selected={new Date(dateStart)}
+                  onChange={(date) => handleInputChange('dateStart', date)}
+                />
+              </div>
+
+              <div className="field">
+                <label>End date</label>
+                <DatePicker
+                  dateFormat="MMMM d, yyyy"
+                  selected={new Date(dateEnd)}
+                  onChange={(date) => handleInputChange('dateEnd', date)}
+                />
+              </div>
+
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
       </Form>
     );
