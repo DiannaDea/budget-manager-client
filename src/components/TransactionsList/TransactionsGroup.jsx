5a14edc13ee0d572e8cef298ebf4489bb49f986d
Diana Baburina <dbaburina.consultant@newscorp.com>
2020-05-31 17:57:56 +0300

    Add sum amount

--- src/components/TransactionsList/TransactionsGroup.jsx
+++ src/components/TransactionsList/TransactionsGroup.jsx
@@ -1,15 +1,38 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-return-assign */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { Segment, Divider } from 'semantic-ui-react';
+import {
+  Segment, Divider, Grid, Header,
+} from 'semantic-ui-react';
 import TransactionsItem from './TransactionItem';
 
 export default class TransactionsGroups extends Component {
+  getSumAmount = (transactions) =>
+    transactions.reduce((acc, cur) => acc += cur.operationAmount, 0)
+
   render() {
     const { date, transactions } = this.props;
+    const sumAmount = this.getSumAmount(transactions);
+
+    const amountStyles = {
+      color: (sumAmount < 0) ? '#e05959' : '#33a933',
+    };
 
     return (
       <Segment>
-        <p>{date}</p>
+        <Grid padded>
+          <Grid.Row columns={2}>
+            <Grid.Column textAlign="left">
+              <p>{date}</p>
+            </Grid.Column>
+            <Grid.Column textAlign="right">
+              <Header style={amountStyles} as="h3">{`${sumAmount} UAH`}</Header>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider />
         {
           transactions.map((transaction, index) => (
             <>
